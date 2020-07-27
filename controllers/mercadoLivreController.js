334bf43edf7a49f427c3626f936c67787b4681d4
Madson de Carvalho <madson.de.carvalho@gmail.com>
2020-07-27 14:37:24 -0300

    fazendo web scrapping nas páginas do mercado livre

---
+++ controllers/mercadoLivreController.js
@@ -0,0 +1,67 @@
+
+const cheerio = require('cheerio');
+const axios = require('axios');
+const urlMercadoLivre = "https://lista.mercadolivre.com.br/"
+ 
+//função de apoio para pegar o html como string
+const getHTML = async function (url){
+    try {
+        const { data } = await axios.get(url);
+        return data;
+      } catch {
+        console.error(
+          `ERROR: An error occurred while trying to fetch the URL: ${url}`
+        );
+      }
+}
+
+
+const search = async (req, res) => {
+
+    const {search, limit} = req.body
+    const urlPesquisa = `${urlMercadoLivre}${search}#D[A:${search}]`
+    
+    let html = await getHTML(urlPesquisa)
+    const $ = cheerio.load(html)
+
+    let result = [] //array que será enviado na resposta
+
+    //primeiramente precisamos saber se estamos em uma página tipo grid ou lista
+    let gridListLength = $(".item__info-link").length
+    
+    if(gridListLength > 0){
+        //percorrer itens pegando seus dados
+        $(".item__info-link").each(
+            (index, el) => {
+                const name = $(el).find('.main-title').text().trim()
+                const price = $(el).find('.price__fraction').text().trim()
+                const store = $(el).find('.item__brand-title-tos').text().trim().replace('por ', '')
+                const link = $(el).attr('href')
+                const state = $(el).find('.item__condition').text().trim()
+
+                result.push({name : name, link: link, price: price, store:store, state: state})
+            }
+        )
+    }else {
+        //caso o gridListLength seja igual a zero, provavelmente estamos em uma pagina tipo lista
+        //percorrendo uma página tipo lista
+        $(".results-item").each(
+            (index, el) => {
+                const name = $(el).find('.item__title').text().trim()
+                const price_fraction = $(el).find('.price__fraction').text().trim()
+                const price_decimals = $(el).find('.price__decimals').text().trim()
+                const price = `${price_fraction},${price_decimals}`
+                const store = $(el).find('.item__brand-title-tos').text().trim().replace('por ', '')
+                const link = $(el).find('a.item__js-link').attr('href')
+                const state = $(el).find('.item__condition').text().trim()
+
+                result.push({name : name, link: link, price: price, store:store, state: state})
+            }
+        )
+    }
+    
+
+    return res.status(200).json(result)
+}
+
+module.exports = {search}
--- routes/mercadoLivre.js
+++ routes/mercadoLivre.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const mercadoLivreController = require('../controllers/mercadoLivreController')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('mercado_livre');
+  res.render('mercado_livre')
 });
+
+router.post('/search', mercadoLivreController.search)
 
 
 module.exports = router;
